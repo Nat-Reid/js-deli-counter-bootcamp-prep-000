a1490cadeead115aac7ccc67d75fe962b37a57db
Nat Reid <nathanielreid@gmail.com>
2018-10-13 22:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, customer){
   line.push(customer);
-  var message = "Welcome, "+customer+". You are number "+line.length" in line.";
+  var message = "Welcome, "+customer+". You are number "+line.length+" in line.";
   return message;
 }
