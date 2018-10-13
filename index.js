f3cebc502331709cc72148cbd262bf5fd9863978
Nat Reid <nathanielreid@gmail.com>
2018-10-13 22:51:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ function takeANumber(line, customer){
   var message = "Welcome, "+customer+". You are number "+line.length+" in line.";
   return message;
 }
+function nowServing(line){
+  if (line.size>0){
+    var serving = line.shift();
+    return serving;
+  }
+  else{
+    return "There is nobody waiting to be served!";
+  }
+}
+function currentLine(line){
+  var message = "The line is currently:";
+  for (let i=0; i<line.length; i++){
+    message += i+". "+line[i];
+  }
+  return message;
+}
